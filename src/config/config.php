703b45541376f16c8b5fb13faf4e339e03607750
sara hasan <sara.hasan@nextgeni.com>
2022-07-04 16:10:46 +0500

    Updated bsecure config environment file doc

--- src/config/config.php
+++ src/config/config.php
@@ -3,6 +3,6 @@
 return [
     'client_id' => env('BSECURE_CLIENT_ID', ''),
     'client_secret' => env('BSECURE_CLIENT_SECRET', ''),
-    //use 'production' for live orders and 'sandbox' for testing orders. When left empty or `null` the sandbox environment will be used
+    //use 'live' for live orders and 'sandbox' for testing orders. When left empty or `null` the sandbox environment will be used
     'environment' => env('BSECURE_ENVIRONMENT'),
 ];
